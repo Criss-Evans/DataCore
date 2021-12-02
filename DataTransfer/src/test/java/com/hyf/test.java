17e982472b1449eb178d0b36fd1d046133d0fd7d
lijiaxing <lijiaxing1@lvshou.com>
2021-12-02 18:23:23 +0800

    读取配置文件测试

--- DataTransfer/src/test/java/com/hyf/test.java
+++ DataTransfer/src/test/java/com/hyf/test.java
@@ -7,17 +7,16 @@ import java.io.InputStream;
 import java.util.List;
 import java.util.Properties;
 
-@Slf4j
 public class test {
 
     public static void main(String[] args) {
 
         Properties properties = new Properties();
-        InputStream in = Object.class.getResourceAsStream("core.properties");
+        InputStream in = Object.class.getResourceAsStream("/core.properties");
         try{
             properties.load(in);
         }catch (IOException e) {
-            log.error("读取配置文件失败...", e);
+            e.printStackTrace();
         }
         System.out.println(properties.get("username"));
     }
